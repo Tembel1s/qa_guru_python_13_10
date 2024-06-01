e8c19aa1d5aa87fe58a1ec8359176561bb920187
Sergey Melnikov <tembelis@bk.ru>
2024-06-01 15:21:38 +0300

    Добавлен test_with_steps.py в VCS

---
+++ tests/test_with_steps.py
@@ -0,0 +1,25 @@
+import allure
+from selene.support import by
+from selene.support.conditions import be
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+
+
+def test_dynamic_steps():
+    with allure.step("Открываем главную страницу"):
+        browser.open("https://github.com")
+
+    with allure.step("Ищем репозиторий"):
+        s(".search-input").click()
+        s("#query-builder-test").send_keys("eroshenkoam/allure-example")
+        s("#query-builder-test").submit()
+
+    with allure.step("Переходим по ссылке репозитория"):
+        s(by.link_text("eroshenkoam/allure-example")).click()
+
+    with allure.step("Открываем таб Issues"):
+        s("#issues-tab").click()
+
+    with allure.step("Проверяем наличие строки с заголовком 'новая Issue'"):
+        s(by.partial_link_text("issue")).should(be.visible)
+
